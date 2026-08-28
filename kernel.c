fdfcb595866e9c884575405ca78d6e01f23905f0
anshika <anshikajain196872@gmail.com>
2026-08-28 14:03:32 +0530

    add reusable print() function with cursor tracking

--- kernel.c
+++ kernel.c
@@ -1,11 +1,19 @@
-void kernel_main(void) {
-    char *memory = (char*) 0xB8000;
-    const char *message = "hello from OS";
+char *memory = (char*) 0xB8000;
+int cursor_row = 0;
+int cursor_col = 0;
 
+void print(char *message) {
     for (int i = 0; message[i] != '\0'; ++i) {
-        memory[i * 2] = message[i];
-        memory[i * 2 + 1] = 0x07;
+        int index = (cursor_row * 80 + cursor_col) * 2;
+        memory[index] = message[i];
+        memory[index + 1] = 0x07;
+        cursor_col++;
     }
+}
 
-    while (1);
+void kernel_main(void) {
+    print("Hello m OS!");
+    print(" This is a test.");
+
+    while (1) { }
 }
